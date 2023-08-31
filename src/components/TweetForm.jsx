c2c4204972cc5736b2333c6fb6f97764663b08ab
raghavendra-10 <vattikutiraghavendra3@gmail.com>
2023-08-31 16:10:38 +0530

    adding expanding

--- src/components/TweetForm.jsx
+++ src/components/TweetForm.jsx
@@ -1,42 +1,66 @@
-import React, { useState, useRef } from 'react';
-import { db, storage } from '../firebaseConfig';
-import { collection, addDoc } from 'firebase/firestore';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { BsImage } from 'react-icons/bs';
-import { FcCheckmark} from 'react-icons/fc';
-
-
+import React, { useState, useRef ,useEffect} from "react";
+import { db, storage } from "../firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { BsImage } from "react-icons/bs";
+import { FcCheckmark } from "react-icons/fc";
+import { FaSpinner } from "react-icons/fa";
+import { getDocs, where, query } from 'firebase/firestore';
 const TweetForm = ({ user, profilePhotoURL, onClose }) => {
-  const [tweet, setTweet] = useState('');
+  const [tweet, setTweet] = useState("");
   const [tweetPhoto, setTweetPhoto] = useState(null);
-  const inputFileRef = useRef(null); // Reference to the input element
+  const inputFileRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  useEffect(() => {
+    async function fetchProfilePhotoURL() {
+      if (!user.uid) return;
 
+      const profilesCollection = collection(db, 'profiles');
+      const q = query(profilesCollection, where('uid', '==', user.uid));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        const profileData = querySnapshot.docs[0].data();
+        setUsername(profileData.username || '');
+      }
+    }
+
+    fetchProfilePhotoURL();
+  }, [user.uid]);
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setIsLoading(true);
     try {
-      let tweetPhotoURL = '';
+      let tweetPhotoURL = "";
       if (tweetPhoto) {
-        const storageRef = ref(storage, `tweet-photos/${user.uid}_${Date.now()}`);
+        const storageRef = ref(
+          storage,
+          `tweet-photos/${user.uid}_${Date.now()}`
+        );
         await uploadBytes(storageRef, tweetPhoto);
         tweetPhotoURL = await getDownloadURL(storageRef);
       }
-      await addDoc(collection(db, 'tweets'), {
+
+      await addDoc(collection(db, "tweets"), {
         content: tweet,
         authorId: user.uid,
         authorEmail: user.email,
         createdAt: new Date(),
+        username: username, 
         tweetPhotoURL: tweetPhotoURL,
       });
 
-      setTweet('');
+      setTweet("");
       setTweetPhoto(null);
-      toast.success('Tweeted Successfully');
+      toast.success("Tweeted Successfully");
       onClose(); // Close the modal after successful tweet
     } catch (error) {
       toast.error(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -50,9 +74,13 @@ const TweetForm = ({ user, profilePhotoURL, onClose }) => {
     <div className="">
       <form onSubmit={handleSubmit}>
         {profilePhotoURL && (
-          <img src={profilePhotoURL} alt="Profile" className="w-12 h-12 rounded-full mr-3" />
+          <img
+            src={profilePhotoURL}
+            alt="Profile"
+            className="w-12 h-12 rounded-full mr-3"
+          />
         )}
-        
+
         <textarea
           value={tweet}
           onChange={(e) => setTweet(e.target.value)}
@@ -63,19 +91,20 @@ const TweetForm = ({ user, profilePhotoURL, onClose }) => {
         />
         <div className="flex justify-between w-full">
           <div className="relative flex items-center">
-            <div className="icon-container cursor-pointer px-4 py-3 bg-blue-300 rounded-md hover:bg-pink-400" onClick={handleIconClick}>
+            <div
+              className="icon-container cursor-pointer px-4 py-3 bg-blue-300 rounded-md hover:bg-pink-400"
+              onClick={handleIconClick}
+            >
               <BsImage className="upload-icon text-xl" />
             </div>
             {tweetPhoto && (
-              <span className="ml-2">
-              {<FcCheckmark size={19}/>}
-              </span>
+              <span className="ml-2">{<FcCheckmark size={19} />}</span>
             )}
             <input
               type="file"
               accept="image/*"
               ref={inputFileRef}
-              style={{ display: 'none' }}
+              style={{ display: "none" }}
               onChange={(e) => setTweetPhoto(e.target.files[0])}
             />
           </div>
@@ -90,14 +119,18 @@ const TweetForm = ({ user, profilePhotoURL, onClose }) => {
             <button
               type="submit"
               className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-blue-600 ml-2"
+              disabled={isLoading}
             >
-              Go
+              {isLoading ? (
+                <>
+                  <FaSpinner size={20} className="animate-spin ml-1" />
+                </>
+              ) : (
+                "GO"
+              )}
             </button>
           </div>
         </div>
-
-          
-        
       </form>
     </div>
   );
