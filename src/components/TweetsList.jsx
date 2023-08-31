1663a3f25f1a7cd0a5c66282938f84ea1c046c60
raghavendra-10 <vattikutiraghavendra3@gmail.com>
2023-08-31 12:11:17 +0530

    added notify

--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -1,5 +1,5 @@
 // TweetsList.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useCallback } from 'react';
 import { db } from '../firebaseConfig';
 import { collection, onSnapshot } from 'firebase/firestore';
 import { getDocs, where, query } from 'firebase/firestore';
@@ -8,6 +8,22 @@ import Tweet from './Tweet';
 
 const TweetsList = () => {
   const [tweets, setTweets] = useState([]);
+
+  const showNotification = useCallback((newTweetsCount) => {
+    if (Notification.permission === 'granted') {
+      const options = {
+        body: `${newTweetsCount} new tweets added.`,
+        icon: '/path/to/notification-icon.png', // Replace with your notification icon
+      };
+      new Notification('New Tweets', options);
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          showNotification(newTweetsCount);
+        }
+      });
+    }
+  }, []);
 
   useEffect(() => {
     const tweetsCollection = collection(db, 'tweets');
@@ -40,14 +56,17 @@ const TweetsList = () => {
 
       // Sort tweets by createdAt in descending order
       tweetsData.sort((a, b) => b.createdAt?.toMillis() - a.createdAt?.toMillis());
-
+      const newTweetsCount = tweetsData.length - tweets.length;
       setTweets(tweetsData);
+      if (newTweetsCount > 0) {
+        showNotification(newTweetsCount);
+      }
     }, (error) => {
       console.error('Error fetching tweets:', error); // Handle error
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [tweets.length, showNotification]);
 
   return (
     <div className='pt-2'>
