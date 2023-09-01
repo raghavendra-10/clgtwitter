8903c17f37e688ba8974db4f4f2147a7abfbec23
raghavendra-10 <vattikutiraghavendra3@gmail.com>
2023-09-01 08:56:24 +0530

    added delete for only admin

--- src/components/CalanderComponent.jsx
+++ src/components/CalanderComponent.jsx
@@ -7,13 +7,36 @@ import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import EventForm from './EventForm';
 import './calender.css';
 import { MdDelete } from 'react-icons/md';
+import { UserAuth } from '../context/AuthContext'; // Assuming you have an AuthContext for user authentication
+
 const localizer = momentLocalizer(moment);
 
 const CalendarComponent = () => {
+  const { user } = UserAuth(); // Get the authenticated user from context
   const [events, setEvents] = useState([]);
   const [showEventForm, setShowEventForm] = useState(false);
   const [selectedEventId, setSelectedEventId] = useState(null);
+ 
+  const [adminUid, setAdminUid] = useState("");
+  useEffect(() => {
 
+
+    async function fetchAdminUid() {
+      try {
+        const adminsCollection = collection(db, "admins");
+        const adminsQuerySnapshot = await getDocs(adminsCollection);
+        if (!adminsQuerySnapshot.empty) {
+          const adminData = adminsQuerySnapshot.docs[0].data();
+          setAdminUid(adminData.adminUid || "");
+         
+        }
+      } catch (error) {
+        console.error("Error fetching admin UID:", error);
+      }
+    }
+
+    fetchAdminUid();
+  }, [user.uid, adminUid]);
   const fetchEvents = async () => {
     try {
       const eventsCollection = collection(db, 'events');
@@ -53,12 +76,14 @@ const CalendarComponent = () => {
 
   return (
     <div className="calendar-container">
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-md mb-4"
-        onClick={() => setShowEventForm(true)}
-      >
-        Add Event
-      </button>
+      {user.uid === adminUid && ( // Assuming you have adminUid available
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md mb-4"
+          onClick={() => setShowEventForm(true)}
+        >
+          Add Event
+        </button>
+      )}
       {showEventForm && (
         <div className="fixed backdrop-blur-md inset-0 flex justify-center items-center bg-gray-800 z-50">
           <div className="bg-white p-4 rounded-md shadow-md w-2/3">
@@ -81,7 +106,7 @@ const CalendarComponent = () => {
           event: ({ event }) => (
             <div>
               <div>{event.title}</div>
-              <button onClick={() => handleDeleteEvent(event.id)}><MdDelete size={20} /></button>
+              <button onClick={() => handleDeleteEvent(event.id)}>{user.uid===adminUid &&(<MdDelete size={20} />)}</button>
             </div>
           ),
         }}
