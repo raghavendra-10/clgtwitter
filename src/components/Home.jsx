388de23e93a39b1bd34a16e648e24543ae2ad172
raghavendra-10 <vattikutiraghavendra3@gmail.com>
2023-10-04 12:48:17 +0530

    animatn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Lottie from "react-lottie";
-import home from ".././components/lottiefile/home.json";
+import Logo from "../assests/newlogo.png";
 
 const Home = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: home,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
-
   return (
     <div>
       <Navbar />
-      <div className="bg-cover bg-center  w-full flex justify-center min-h-screen relative">
-        {/* Lottie animation */}
-        <Lottie options={defaultOptions} style={{ position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' }} />
-
-        {/* Get Started division */}
-        <div className="absolute z-10 flex flex-col items-center justify-center w-full h-full">
-          <div className="bg-white p-8 bg-opacity-50 rounded-lg shadow-lg text-center">
-            <div className="sm:text-5xl text-2xl font-bold  text-gray-800 mb-4">
-              <span className="text-green-400 ">DI</span>VULGE
+      <div className="bg-cover bg-center w-full flex justify-center  min-h-screen"style={{ backgroundImage: `url(${Logo})` }}>
+        <div className=" flex flex-col items-center justify-center ">
+          {/* <div className="text-5xl font-bold text-gray-800 mb-4">DIVULGE</div> */}
+          <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+            <div className="sm:text-5xl font-bold text-gray-800 mb-4">
+              <span className="text-green-400">DI</span>VULGE
             </div>
-            <div className="sm:text-xl text-navy-600 mb-8">
+            <div className="sm:text-xl text-gray-600 mb-8">
               It's time to communicate
             </div>
             <button className="sm:px-6 sm:py-3 px-1 py-1 sm:text-lg font-semibold text-white bg-blue-500 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200">
@@ -34,6 +21,10 @@ const Home = () => {
             </button>
           </div>
         </div>
+        {/* <div
+          className="bg-cover bg-center w-full  "
+          style={{ backgroundImage: `url(${Logo})` }}
+        /> */}
       </div>
     </div>
   );
